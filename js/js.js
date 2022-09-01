37995a5cb028f1f6616d7fa043566e31c31adfe8
Philippe <philippe.vial90@gmail.com>
2022-09-01 11:50:26 +0200

    Major changes

--- js/js.js
+++ js/js.js
@@ -1,15 +1,4 @@
 "use strict";
-// function myFunction(event: Event) {
-//     let x: HTMLButtonElement | null = document.querySelector('#myTopnav');
-//     console.log('körs', x);
-//     if(x) {
-//         if (x.className === "topnav") {
-//         x.className += " responsive";
-//         } else {
-//         x.className = "topnav";
-//         }
-//     }
-//   }
 const getElement = (selector) => {
     const element = document.querySelector(selector);
     console.log(element);
@@ -18,7 +7,35 @@ const getElement = (selector) => {
     throw Error(`Please double check your class names, there is no ${selector} class`);
 };
 const links = getElement('.nav-links');
-const navBtnDOM = getElement('.nav-btn');
-navBtnDOM.addEventListener('click', () => {
+const navBtn = getElement('.nav-btn');
+navBtn.addEventListener('click', () => {
     links.classList.toggle('show-links');
 });
+const elRecipeNameOutput = document.querySelector('#recipe-name');
+const elInstructionsOutput = document.querySelector('#instructions-output');
+const elCookTimeOutput = document.querySelector('#cookTime-output');
+const elAboutContainer = document.querySelector('#about-container');
+const elAboutHead = document.querySelector('#about-head');
+const elDescText1 = document.querySelector('#desc-text1');
+const elDescText2 = document.querySelector('#desc-text2');
+function fetchText() {
+    fetch('/assets/recipes/boiled-egg.json')
+        .then(response => response.json()).then(data => {
+        if (elDescText1 && elDescText2 && elAboutContainer && elAboutHead) {
+            let head = data.header;
+            let text1 = data.descText1;
+            let text2 = data.descText2;
+            let image = data.imgSrc;
+            elAboutHead.textContent = head;
+            elDescText1.textContent = text1;
+            elDescText2.textContent = text2;
+            const img = document.createElement("img");
+            img.src = image;
+            img.className = "cust-aboutImg";
+            elAboutContainer === null || elAboutContainer === void 0 ? void 0 : elAboutContainer.appendChild(img);
+        }
+        else {
+            console.error('Something went wrong(fetchText)');
+        }
+    });
+}
--- js/js.ts
+++ js/js.ts
@@ -1,19 +1,3 @@
-
-// function myFunction(event: Event) {
-//     let x: HTMLButtonElement | null = document.querySelector('#myTopnav');
-
-//     console.log('körs', x);
-//     if(x) {
-//         if (x.className === "topnav") {
-//         x.className += " responsive";
-//         } else {
-//         x.className = "topnav";
-//         }
-//     }
-
-//   }
-
-
 const getElement = (selector: any) => {
     const element = document.querySelector(selector)
     console.log(element);
@@ -24,8 +8,44 @@ const getElement = (selector: any) => {
   }
   
   const links = getElement('.nav-links')
-  const navBtnDOM = getElement('.nav-btn')
+  const navBtn = getElement('.nav-btn')
   
-  navBtnDOM.addEventListener('click', () => {
+  navBtn.addEventListener('click', () => {
     links.classList.toggle('show-links')
   })
+
+
+const elRecipeNameOutput: HTMLHeadElement | null = document.querySelector('#recipe-name')
+const elInstructionsOutput: HTMLParagraphElement | null = document.querySelector('#instructions-output')
+const elCookTimeOutput: HTMLParagraphElement | null = document.querySelector('#cookTime-output')
+const elAboutContainer = document.querySelector('#about-container')
+const elAboutHead: HTMLHeadElement | null = document.querySelector('#about-head')
+const elDescText1: HTMLParagraphElement | null = document.querySelector('#desc-text1')
+const elDescText2: HTMLParagraphElement | null = document.querySelector('#desc-text2')
+
+
+function fetchText(){
+  fetch('/assets/recipes/boiled-egg.json')
+    
+    .then(response => response.json()).then(data => {
+        if(elDescText1 && elDescText2 && elAboutContainer && elAboutHead) {
+        let head = data.header;
+        let text1 = data.descText1;
+        let text2 = data.descText2;
+        let image = data.imgSrc;
+        
+
+        elAboutHead.textContent = head;
+        elDescText1.textContent = text1;
+        elDescText2.textContent = text2;
+        const img = document.createElement("img");
+        img.src = image;
+        img.className="cust-aboutImg";
+        elAboutContainer?.appendChild(img);
+
+    } else {
+      console.error('Something went wrong(fetchText)')
+    }
+
+    });
+  }
